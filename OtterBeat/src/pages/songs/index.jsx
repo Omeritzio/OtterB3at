0b95f68f46b4b3afdfaa74707ba312e465f0d8ec
omeritzio <omershlomo441@gmail.com>
2023-05-23 12:14:26 +0300

    Second Commit

--- OtterBeat/src/pages/songs/index.jsx
+++ OtterBeat/src/pages/songs/index.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
 
 function songsPage() {
-  return (
-    <div>songsPage</div>
-  )
+  return(
+  <div>songsPage</div>
+  ) ;
 }
 
-export default songsPage
+export default songsPage;
