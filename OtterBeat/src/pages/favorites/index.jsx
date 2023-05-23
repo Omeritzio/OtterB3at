f6ae5e159703efe3655fc717ace286845c55abce
omeritzio <omershlomo441@gmail.com>
2023-05-23 12:08:47 +0300

    Second Commit

---
+++ OtterBeat/src/pages/favorites/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function favoritePage() {
+  return (
+    <div>index</div>
+  )
+}
+
+export default favoritePage
---
+++ OtterBeat/src/pages/songs/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function songsPage() {
+  return (
+    <div>songsPage</div>
+  )
+}
+
+export default songsPage
